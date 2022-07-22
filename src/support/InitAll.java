4b7e8963c722542e204013137f1bb876fc3792a5
Anton Kutnyashenko <a_kutnyashenko@mail.ru>
2022-07-22 22:30:42 +0700

    ClassPath

--- src/support/InitAll.java
+++ src/support/InitAll.java
@@ -4,8 +4,11 @@ package support;
 import com.google.common.reflect.ClassPath;
 import org.reflections.Reflections;
 import wildLife.Animals;
+import wildLife.predators.Predator;
 
+import java.io.IOException;
 import java.util.*;
+import java.util.function.Predicate;
 
 public class InitAll {
 
@@ -14,36 +17,16 @@ public class InitAll {
 
     public static Map<String, Integer> initAllAtIsland() {
 
-
-
-
-        Reflections reflections = new Reflections("java.util");
-        Set<Class<? extends List>> classes = reflections.getSubTypesOf(java.util.List.class);
-        for (Class<? extends List> aClass : classes) {
-            System.out.println(aClass.getName());
-            if(aClass == ArrayList.class) {
-                List list = null;
-                try {
-                    list = aClass.newInstance();
-                } catch (InstantiationException e) {
-                    throw new RuntimeException(e);
-                } catch (IllegalAccessException e) {
-                    throw new RuntimeException(e);
-                }
-                list.add("test");
-                System.out.println(list.getClass().getName() + ": " + list.size());
-            }
-        }
-
-
-
-//        Class clazz = Animals.class;
-//        Reflections reflections = new Reflections("Animals", new SubTypesScanner());
-//        Set<Animals> animals = reflections.getSubTypesOf(clazz);
         Map<String, Integer> all = new HashMap<>();
-//        for (String s : all.keySet()) {
-//            all.put(s.getClass().getSimpleName(), 1);
-//        }
-        return all;
+
+        try {
+            List<Animals> list = ClassPath.from(Animals.class.getClassLoader()).getAllClasses().stream()
+                    .filter()
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+
+
+            return all;
+        }
     }
 }
